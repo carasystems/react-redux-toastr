12fa4d47323ad1f026b5be2f914f7921872b6158
isaac <isaac@verifly.com>
2018-10-02 22:36:54 +0800

    update

--- src/reducer.js
+++ src/reducer.js
@@ -1,4 +1,5 @@
-import {createReducer, guid}  from './utils.js';
+import { fromJS } from 'immutable';
+import { guid }  from './utils.js';
 import config from './config';
 import {
   ADD_TOASTR,
@@ -13,84 +14,64 @@ import {
 // We will cache data so we can check for duplicated before fire the add action.
 export let toastrsCache = [];
 
-const initialState = {
+const initialState = fromJS({
   toastrs: [],
   confirm: null
-};
-
-export default createReducer(initialState, {
-  [ADD_TOASTR]: (state, toastr) => {
-    if (toastr.ignoreToastr) {
-      return state;
-    }
-
-    const newToastr = {
-      id: guid(),
-      position: config.position,
-      ...toastr
-    };
-
-    let newState = {};
-    if (!config.newestOnTop) {
-      newState = {
-        ...state,
-        toastrs: [
-          ...state.toastrs,
-          newToastr
-        ]
-      };
-    } else {
-      newState = {
-        ...state,
-        toastrs: [
-          newToastr,
-          ...state.toastrs
-        ]
-      };
-    }
-    toastrsCache = newState.toastrs;
-    return newState;
-  },
-  [REMOVE_TOASTR]: (state, id) => {
-    let newState = {
-      ...state,
-      toastrs: state.toastrs.filter(toastr => toastr.id !== id)
-    };
-
-    toastrsCache = newState.toastrs;
-    return newState;
-  },
-  [REMOVE_BY_TYPE]: (state, type) => {
-    let newState = {
-      ...state,
-      toastrs: state.toastrs.filter(toastr => toastr.type !== type)
-    };
-
-    toastrsCache = newState.toastrs;
-    return newState;
-  },
-  [CLEAN_TOASTR]: (state) => {
-    toastrsCache = [];
-    return {
-      ...state,
-      toastrs: []
-    };
-  },
-  [SHOW_CONFIRM]: (state, {id, message, options}) => {
-    return {
-      ...state,
-      confirm: {
-        id: id || guid(),
-        show: true,
-        message,
-        options: options || {}
-      }
-    };
-  },
-  [HIDE_CONFIRM]: (state) => {
-    return {
-      ...state,
-      confirm: null
-    };
-  }
 });
+
+export default function toastReducer(state = initialState, action) {
+  let toastrs;
+  switch (action.type) {
+    /* istanbul ignore next */
+    case ADD_TOASTR:
+      const toastr = action.payload;
+
+      if (toastr.ignoreToastr) {
+        return state;
+      }
+
+      const newToastr = {
+        id: guid(),
+        ...toastr
+      };
+
+      if (!config.newestOnTop) {
+        toastrs = [
+          ...state.get('toastrs').toJS(),
+          newToastr,
+        ];
+      } else {
+        toastrs = [
+          newToastr,
+          ...state.get('toastrs').toJS(),
+        ];
+      }
+      toastrsCache = newState.toastrs;
+
+      return state.merge({
+        toastrs,
+      });
+    case REMOVE_TOASTR:
+      toastrs = state.get('toastrs').toJS().filter(toastr => toastr.id !== action.payload);
+
+      toastrsCache = toastrs;
+
+      return state.merge({
+        toastrs,
+      });
+    case CLEAN_TOASTR:
+      toastrsCache = [];
+      return state.set('toastrs', []);
+    case SHOW_CONFIRM:
+      return state.set('confirm', {
+        id: action.payload.id || guid(),
+        show: true,
+        message: action.payload.message,
+        options: action.payload.options || {},
+      });
+    case HIDE_CONFIRM:
+      return state.set('confirm', null);
+    default:
+      return state;
+  }
+}
